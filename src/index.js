f059c6d9ec6e65b8c22acde8e31f232e947147ad
Christopher Fudala <cfudala82@me.com>
2017-11-28 14:42:19 -0600

    changed html

--- src/index.js
+++ src/index.js
@@ -35,7 +35,6 @@ class App extends Component {
 
     return (
       <div>
-        <h1>HELLO</h1>
         <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
